6da86ef94897cf46bdeccbef73de6f344a25637d
rvd <nseries_nokia@list.ru>
2026-02-24 18:56:25 +0400

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
+
+        // Задача 2
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
     }
 }
