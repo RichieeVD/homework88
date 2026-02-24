fbf4983f16e3deb9c2990a73200965cd8311fbfd
rvd <nseries_nokia@list.ru>
2026-02-24 18:58:39 +0400

    Задача 7

--- src/Main.java
+++ src/Main.java
@@ -29,5 +29,10 @@ public class Main {
         for (int i = 7; i <= 98; i = i + 7) {
             System.out.print(i + " ");
         }
+
+        // Задача 7
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.print(i + " ");
+        }
     }
 }
